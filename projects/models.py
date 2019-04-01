632eae6b00966831aa8e13f16de5bd4a055f1621
wecode <umurangamclaire@gmail.com>
2019-04-01 10:29:20 +0200

    adding models class

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Profile(models.Model):
+    profile_photo = models.ImageField(upload_to='gram/', blank=True)
+    bio= models.CharField(max_length =1000)
+    contact=models.CharField(max_length =100)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+
+def __str__(self):
+        return self.user_name
+        
+def save_profile(self):
+        self.save()
+
+class Meta:
+        ordering = ['user_name']
+
+def __str__(self):
+        return self.name
