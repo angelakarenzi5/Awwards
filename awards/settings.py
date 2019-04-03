dbe7f92b0940d4dd2864399c285f8696aa927741
wecode <umurangamclaire@gmail.com>
2019-04-03 12:07:14 +0200

    restarting database process of connectinf to postgres

--- awards/settings.py
+++ awards/settings.py
@@ -77,10 +77,10 @@ WSGI_APPLICATION = 'awards.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'awards',
         'USER': 'wecode',
-        'PASSWORD':'wecode',
+    'PASSWORD':'wecode',
     }
 }
 
@@ -109,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Kigali'
 
 USE_I18N = True
 
