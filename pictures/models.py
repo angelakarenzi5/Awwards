fdac65f045488dfe6ef6a585ac2c4dea90988eb9
wecode <umurangamclaire@gmail.com>
2019-04-04 10:31:44 +0200

    importing django validators in models

--- pictures/models.py
+++ pictures/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 import datetime as dt
 from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator,MaxValueValidator
+
 # Create your models here.
 
 class Profile(models.Model):
